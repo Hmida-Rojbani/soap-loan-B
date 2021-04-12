fd08462b85e5681bd323ac460135f34ea24c684a
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-04-12 14:04:38 +0100

    Soap first part

---
+++ src/main/java/de/tekup/loan/SoapLoanBApplication.java
@@ -0,0 +1,13 @@
+package de.tekup.loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapLoanBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapLoanBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/tekup/loan/SoapLoanBApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tekup.loan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapLoanBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
